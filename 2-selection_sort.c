6268e12c40d6c6dd2ac6e7d7e4d3c9c8c24c6b1e
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-06-11 22:05:17 +0000

    task 2 sorting

---
+++ 2-selection_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts an array of integers using the Selection sort algorithm
+ * in ascending order
+ * @array: array to sort
+ * @size: size of array {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, c, tmp, flag;
+
+	for (i = 0; i < size; i++)
+	{
+		flag = 0;
+		for (j = i + 1, c = i; j < size; j++)
+		{
+			if (array[c] > array[j])
+			{
+				c = j;
+				flag = 1;
+			}
+		}
+		if (flag == 1)
+		{
+			tmp = array[i];
+			array[i] = array[c];
+			array[c] = tmp;
+			print_array(array, size);
+		}
+	}
+}
