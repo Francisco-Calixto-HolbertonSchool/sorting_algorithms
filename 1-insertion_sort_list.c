afd73464c5038a21799aa8d0200cfb19664ce98c
rrrorrigo <rodrigobriones54@gmail.com>
2021-06-13 16:51:45 -0300

    fixed betty

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,24 +8,24 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *travel;
 
-        if (!list)
-                return;
-        travel = (*list)->next;
-        for (; travel; travel = travel->next)
-        {
-                for (; travel->prev && travel->n < travel->prev->n;)
-                {
-                        travel->prev->next = travel->next;
-                        if (travel->next)
-                                travel->next->prev = travel->prev;
-                        travel->next = travel->prev;
-                        travel->prev = travel->next->prev;
-                        travel->next->prev = travel;
-                        if (!travel->prev)
-                                *list = travel;
-                        else
-                                travel->prev->next = travel;
-                        print_list(*list);
-                }
-        }
+	if (!list)
+		return;
+	travel = (*list)->next;
+	for (; travel; travel = travel->next)
+	{
+		for (; travel->prev && travel->n < travel->prev->n;)
+		{
+			travel->prev->next = travel->next;
+			if (travel->next)
+				travel->next->prev = travel->prev;
+			travel->next = travel->prev;
+			travel->prev = travel->next->prev;
+			travel->next->prev = travel;
+			if (!travel->prev)
+				*list = travel;
+			else
+				travel->prev->next = travel;
+			print_list(*list);
+		}
+	}
 }
