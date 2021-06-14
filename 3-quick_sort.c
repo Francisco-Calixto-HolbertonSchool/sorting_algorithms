00970928c935a20a5a379cc1f9aee39a09ea2dbe
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-06-14 17:20:20 +0000

    betty

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,14 +1,18 @@
 #include "sort.h"
+
 /**
  * swap - function that swap two elements of an array
- * @arr: array to order
- * @l: Starting index
+ * @i: array to order
+ * @j: Starting index
  */
+
 void swap(int *i, int *j)
 {
-    int tmp = *i;
-    *i = *j;
-    *j = tmp;
+	int tmp;
+
+	tmp = *i;
+	*i = *j;
+	*j = tmp;
 }
 /**
  * partition - function that sorts an array of integers in ascending order
@@ -16,30 +20,32 @@ void swap(int *i, int *j)
  * @l: Starting index
  * @r: ending index
  * @size: size of index
+ * Return: middle of array
  */
+
 int partition(int *arr, int l, int r, size_t size)
 {
-        int pivot = arr[r];
-        int i = l - 1, j;
+	int pivot = arr[r];
+	int i = l - 1, j;
 
-        for (j = l; j < r; j++)
-        {
-                if (arr[j] <= pivot)
-                {
-                        i++;
-                        if (i != j)
-                        {
-                                swap(&arr[i], &arr[j]);
-                                print_array(arr, size);
-                        }
-                }
-        }
-        if (arr[i + 1] > arr[r])
-        {
-                swap(&arr[i + 1], &arr[r]);
-                print_array(arr, size);
-        }
-        return i + 1;
+	for (j = l; j < r; j++)
+	{
+		if (arr[j] <= pivot)
+		{
+			i++;
+			if (i != j)
+			{
+				swap(&arr[i], &arr[j]);
+				print_array(arr, size);
+			}
+		}
+	}
+	if (arr[i + 1] > arr[r])
+	{
+		swap(&arr[i + 1], &arr[r]);
+		print_array(arr, size);
+	}
+	return (i + 1);
 }
 /**
  * rquick_sort - function that sorts an array of integers in ascending order
@@ -50,20 +56,21 @@ int partition(int *arr, int l, int r, size_t size)
  */
 void rquick_sort(int *arr, int l, int r, size_t size)
 {
-        int i;
+	int i;
 
-        if (l >= r)
-                return;
-        i = partition(arr, l, r, size);
-        rquick_sort(arr, l, i - 1, size);
-        rquick_sort(arr, i + 1, r, size);
+	if (l >= r)
+		return;
+	i = partition(arr, l, r, size);
+	rquick_sort(arr, l, i - 1, size);
+	rquick_sort(arr, i + 1, r, size);
 }
 /**
  * quick_sort - function that sorts an array of integers in ascending order
  * @array: array to order
  * @size: size of array
  */
+
 void quick_sort(int *array, size_t size)
 {
-        rquick_sort(array, 0, size - 1, size);
+	rquick_sort(array, 0, size - 1, size);
 }
